1c8eb10a279371ad7fdc2f80a5d8e7708f1b68a5
Pavithra221021 <163543253+Pavithra221021@users.noreply.github.com>
2025-10-23 17:50:25 +0530

    Add files via upload

---
+++ api/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+let books = [
+  { id: 1, title: 'micky', author: 'micky' },
+  { id: 2, title: 'The Jungle Book', author: 'Kipling' },
+  { id: 3, title: 'Alice in Wonderland', author: 'aLewis' }
+];
+
+app.post('/books', (req, res) => {
+  const { title, author } = req.body;
+  if (!title || !author) {
+    return res.status(400).json({ message: 'title and author are required' });
+  }
+  const newBook = { id: books.length + 1, title, author };
+  books.push(newBook);
+  res.status(201).json(newBook);
+});
+
+app.get('/books', (req, res) => {
+  res.status(200).json(books);
+});
+
+module.exports = app;
